ffaa0e5b006d0378decf8192429cfd8f327c5cc6
rusmanov <usmanov.riyaz22@gmail.com>
2023-12-13 17:37:51 +0100

    feat: test improvments

--- src/frontend/src/__tests__/App.test.js
+++ src/frontend/src/__tests__/App.test.js
@@ -4,11 +4,22 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import { render } from '@testing-library/react';
 
-it("should be loading first", () => {
+it("should be home page", () => {
   const { getByText } = render(
     (<Provider store={store}>
       <App />
     </Provider>) 
   );
-  expect(getByText(/loading.../i)).toBeInTheDocument();
+  expect(getByText(/home page/i)).toBeInTheDocument();
+});
+
+it("should be username input", () => {
+  const { getByRole } = render(
+    (<Provider store={store}>
+      <App />
+    </Provider>) 
+  );
+
+  expect(getByRole("textbox")).toBeInTheDocument();
+  expect(getByRole("button")).toBeInTheDocument();
 });
