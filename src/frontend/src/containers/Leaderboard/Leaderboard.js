c32b46334c9487e1de290522b81df017e06553bc
Tatiana Komaristaia <tkomaristaia@gmail.com>
2024-04-12 21:06:56 +0200

    table in leaderboard

--- src/frontend/src/containers/Leaderboard/Leaderboard.js
+++ src/frontend/src/containers/Leaderboard/Leaderboard.js
@@ -1,32 +1,39 @@
 import Link from "../../components/Link";
 import { useGetLeaderboardQuery } from "../../app/api/api";
+import { Button, Flex, Table } from "antd";
 
 function Leaderboard() {
   const { data, isLoading } = useGetLeaderboardQuery();
+  console.log(data);
 
   return (
-    <>
+    <Flex vertical align="center">
       <h1>Leaderboard</h1>
       {isLoading ? (
         <h2>Loading...</h2>
       ) : (
         data && (
-          <ul>
-            {[...data]
-              .sort((a, b) => b.score - a.score)
-              .map((item, ind) => {
-                return (
-                  <li key={ind}>
-                    <p>{item.username}</p>
-                    <p>{item.score}</p>
-                  </li>
-                );
-              })}
-          </ul>
+          <Table
+            columns={[
+              { title: "Player Name", dataIndex: "username" },
+              { title: "Score", dataIndex: "score" },
+            ]}
+            dataSource={data.map(({ username, score }, i) => ({
+              key: i,
+              username,
+              score,
+            }))}
+            bordered={true}
+            pagination={{ pageSize: 5 }}
+            style={{ marginTop: 30 }}
+          />
         )
       )}
-      <Link to="#">Go to Home</Link>
-    </>
+      {/* <Link to="#">Go to Home</Link> */}
+      <Button type="primary" to="#">
+        Home
+      </Button>
+    </Flex>
   );
 }
 
