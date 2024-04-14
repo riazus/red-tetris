b4249f1d0f039bfda16d9032def643579b6b651a
riazus <riyaz@alexlegal.fr>
2024-04-14 21:01:10 +0200

    feat: tetris tests improvements

--- src/frontend/src/containers/Tetris/Tetris.test.js
+++ src/frontend/src/containers/Tetris/Tetris.test.js
@@ -1,11 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
 import gameReducer from "../../app/slices/gameSlice";
 import userReducer from "../../app/slices/playerSlice";
+import { SOCKETS } from "../../const";
+import { createStage } from "../../gameHelpers";
+import { useGameStatus } from "../../hooks/useGameStatus";
+import { usePlayer } from "../../hooks/usePlayer";
+import { useStage } from "../../hooks/useStage";
+import { emitAppSocketEvent } from "../../sockets/socket";
+import { TETROMINOS } from "../../tetrominos";
 import Tetris from "./Tetris";
 
+jest.mock("../../sockets/socket.js", () => ({ emitAppSocketEvent: jest.fn() }));
+
+jest.mock("../../hooks/usePlayer", () => ({ usePlayer: jest.fn() }));
+
+jest.mock("../../hooks/useStage", () => ({ useStage: jest.fn() }));
+
+jest.mock("../../hooks/useGameStatus", () => ({ useGameStatus: jest.fn() }));
+
 describe("Tetris", () => {
+  beforeEach(() => {
+    const defaultPlayer = {
+      pos: { x: 0, y: 0 },
+      tetromino: TETROMINOS[0].shape,
+      ind: 0,
+      collided: false,
+    };
+    usePlayer.mockImplementation(() => [
+      defaultPlayer,
+      jest.fn(),
+      jest.fn(),
+      jest.fn(),
+    ]);
+    useStage.mockImplementation(() => [createStage(), jest.fn(), 0]);
+    useGameStatus.mockImplementation(() => [0, jest.fn(), 0, jest.fn()]);
+  });
+
   it("should have exit room button", () => {
     const testStore = configureStore({
       reducer: { player: userReducer, game: gameReducer },
@@ -52,5 +85,63 @@ describe("Tetris", () => {
     );
 
     expect(screen.getByTestId("restart-game-button")).toBeInTheDocument();
+  });
+
+  it("should have possibility to exit from room", () => {
+    const stage = global.random20Tetrominos();
+    const testStore = configureStore({
+      reducer: { player: userReducer, game: gameReducer },
+      preloadedState: {
+        player: {
+          isAdmin: true,
+        },
+        game: {
+          isGameover: true,
+          isStarted: true,
+          players: [],
+          tetrominos: stage,
+        },
+      },
+    });
+
+    render(
+      <Provider store={testStore}>
+        <Tetris />
+      </Provider>
+    );
+
+    const exitButton = screen.getByTestId("exit-room-button");
+    act(() => exitButton.click());
+    expect(emitAppSocketEvent).toHaveBeenCalledWith(SOCKETS.EXIT_ROOM);
+  });
+
+  it("should have possibility to restart the game", () => {
+    const testStore = configureStore({
+      reducer: { player: userReducer, game: gameReducer },
+      preloadedState: {
+        player: {
+          isAdmin: true,
+        },
+        game: {
+          isGameover: true,
+          isStarted: true,
+          players: [],
+        },
+      },
+    });
+    const setStage = jest.fn();
+    const cleanStage = createStage();
+    useStage.mockImplementation(() => [cleanStage, setStage, 0]);
+
+    render(
+      <Provider store={testStore}>
+        <Tetris />
+      </Provider>
+    );
+
+    const restartButton = screen.getByTestId("restart-game-button");
+    act(() => restartButton.click());
+    expect(emitAppSocketEvent).toHaveBeenCalledWith(SOCKETS.RESTART_GAME);
+    expect(setStage).toHaveBeenCalledWith(cleanStage);
   });
 });
--- src/frontend/src/setupTests.js
+++ src/frontend/src/setupTests.js
@@ -1,5 +1,7 @@
 import "@testing-library/jest-dom";
 
+const TETROMINOS = ["I", "J", "L", "O", "S", "T", "Z"];
+
 global.setImmediate =
   global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
 
@@ -9,3 +11,13 @@ global.matchMedia =
     addListener: jest.fn(),
     removeListener: jest.fn(),
   }));
+
+global.random20Tetrominos = () => {
+  const res = [];
+
+  for (var i = 0; i < 20; i++) {
+    res.push(TETROMINOS[Math.floor(Math.random() * TETROMINOS.length)]);
+  }
+
+  return res;
+};
