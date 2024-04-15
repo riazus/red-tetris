b628472dcf1c19becefd270e71fae0d820d9bcd7
riazus <riyaz@alexlegal.fr>
2024-04-15 22:08:08 +0200

    feat: gameHelpers tests

--- src/frontend/src/gameHelpers.js
+++ src/frontend/src/gameHelpers.js
@@ -5,7 +5,6 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () => Array(STAGE_WIDTH).fill([0, 'clear']));
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-
   for (let y = 0; y < player.tetromino.length; y += 1) {
     for (let x = 0; x < player.tetromino[y].length; x += 1) {
       if (player.tetromino[y][x] !== 0) {
